66a12dd170b806dc8f1d36d4fdb93a1a6bf1ae1a
Jeffrey Buehler <jeffreydeanbuehler@gmail.com>
2016-10-07 12:30:37 -0600

    gcd equals

--- src/RSA.java
+++ src/RSA.java
@@ -2,11 +2,12 @@
 public class RSA {
 
 	public static void main(String[] args) {
-		System.out.println(gcd(73546,19712));
-		System.out.println(gcd(73546,19509));
-		System.out.println(gcd(100, 9));
-		System.out.println(gcd(0, 5));
-		System.out.println(gcd(-2, 8));
+		gcd_equals(59, 10200, 1);
+//		System.out.println(gcd(73546,19712));
+//		System.out.println(gcd(73546,19509));
+//		System.out.println(gcd(100, 9));
+//		System.out.println(gcd(0, 5));
+//		System.out.println(gcd(-2, 8));
 	}
 
 	public static int gcd (int a, int b) {
@@ -21,8 +22,31 @@ public class RSA {
 			return a;
 		if (a == 0)
 			return 1;
-		int multiplier = a / b;
-		int remainder = a - (b * multiplier);
-		return gcd(b, remainder);
+		return gcd(b, a % b);
+	}
+
+	public static boolean gcd_equals(int a, int b, int equals) {
+		a = Math.abs(a);
+		b = Math.abs(b);
+		if (b > a) {
+			int temp = a;
+			a = b;
+			b = temp;
+		} // a is always the bigger number
+		gcd_equals(a, b, equals, 1, 0, 0, 1, a, b);
+		return true;
+	}
+
+	public static int gcd_equals(int a, int b, int equals, int aa, int ab, int ba, int bb, int initial_a, int initial_b) {
+		if (a == 1 || b == 0)
+			return a;
+		if (b == equals) {
+			System.out.println(equals + " = (" + ba + ") * " + initial_a + " + (" + bb + ") * " + initial_b);
+		}
+		int multiplier = a / b * -1;
+		int current = a + b * multiplier;
+		int ca = aa + ba * multiplier;
+		int cb = ab + bb * multiplier;
+		return gcd_equals(b, current, equals, ba, bb, ca, cb, initial_a, initial_b);
 	}
 }
