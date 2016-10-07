3033b9513d645befde8936c894685da2a884d3c7
Jeffrey Buehler <jeffreydeanbuehler@gmail.com>
2016-10-07 10:20:24 -0600

    considering negative gcd

--- src/RSA.java
+++ src/RSA.java
@@ -4,9 +4,14 @@ public class RSA {
 	public static void main(String[] args) {
 		System.out.println(gcd(73546,19712));
 		System.out.println(gcd(73546,19509));
+		System.out.println(gcd(100, 9));
+		System.out.println(gcd(0, 5));
+		System.out.println(gcd(-2, 8));
 	}
 
 	public static int gcd (int a, int b) {
+		a = Math.abs(a);
+		b = Math.abs(b);
 		if (b > a) {
 			int temp = a;
 			a = b;
